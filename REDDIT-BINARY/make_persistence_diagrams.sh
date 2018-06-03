b7b741078fb8942633d88bb853cab1e8680d1ad3
Bastian Rieck <bastian@rieck.ru>
2018-06-03 16:09:41 +0200

    Simple script for creating persistence diagrams of reddit data

---
+++ REDDIT-BINARY/make_persistence_diagrams.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+mkdir -p "Persistence diagrams/Inverted"
+mkdir -p "Persistence diagrams/Normal"
+
+OUT_NORMAL="Persistence diagrams/Normal"
+OUT_INVERTED="Persistence diagrams/Inverted"
+BIN=network_analysis
+
+for file in GML/*.gml; do
+  ${BIN} --normalize --output ${OUT_NORMAL}          $file 2
+  ${BIN} --normalize --output ${OUT_NORMAL} --invert $file 2
+done
