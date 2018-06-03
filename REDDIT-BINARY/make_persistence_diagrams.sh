f3aa9932abbf89c73a05239dcdcad47f6a1ddec2
Bastian Rieck <bastian@rieck.ru>
2018-06-03 16:11:12 +0200

    Fixed overwriting problem

--- REDDIT-BINARY/make_persistence_diagrams.sh
+++ REDDIT-BINARY/make_persistence_diagrams.sh
@@ -8,6 +8,6 @@ OUT_INVERTED="Persistence diagrams/Inverted"
 BIN=network_analysis
 
 for file in GML/*.gml; do
-  ${BIN} --normalize --output ${OUT_NORMAL}          $file 2
-  ${BIN} --normalize --output ${OUT_NORMAL} --invert $file 2
+  ${BIN} --normalize --output ${OUT_NORMAL}            $file 2
+  ${BIN} --normalize --output ${OUT_INVERTED} --invert $file 2
 done
